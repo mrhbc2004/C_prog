5cee498723b03f51ae532e96bebc67bd552698f8
Harisha B C <mrhbc2004@gmail.com>
2025-09-24 20:38:52 +0530

    Create In1.c

---
+++ Pointer_structures/In1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n;
+    printf("Enter the number of integers:");
+    scanf("%d",&n);
+    int *ptr=(int *)malloc(n*sizeof(int));
+    if(ptr==NULL){
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    // printf("%d\n",ptr);
+    for(int i=0;i<n;i++){
+        printf("Enter the %d element:",i+1);
+        scanf("%d",(ptr+i));
+    }
+    // printf("%d\n",ptr);
+    for(int i=0;i<n;i++){
+        printf("%d\t",*(ptr+i));
+    }
+    return 0;
+}
